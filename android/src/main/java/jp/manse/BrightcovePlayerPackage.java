a062c8cb81d35d2f2c85ebca48c6bea7299cfebb
Palani Arunagiri <parunagiri@brightcove.com>
2022-02-04 15:18:36 +0530

    Updated build.gradle as per latest version

--- android/src/main/java/jp/manse/BrightcovePlayerPackage.java
+++ android/src/main/java/jp/manse/BrightcovePlayerPackage.java
@@ -1,6 +1,7 @@
 package jp.manse;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -12,6 +13,11 @@ public class BrightcovePlayerPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
         return Arrays.<NativeModule>asList(new BrightcovePlayerUtil(reactApplicationContext));
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return null;
     }
 
     @Override
